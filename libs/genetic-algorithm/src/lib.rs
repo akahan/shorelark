7035bc36c4ae75f2069580331fca07fee7309282
Alexander Stein <alexander.stein@mailbox.org>
2022-07-02 08:41:01 +0200

    Use type_alias_impl_trait feature
    min_type_alias_impl_trait has been removed in favor of
    full type_alias_impl_trait

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(min_type_alias_impl_trait)]
+#![feature(type_alias_impl_trait)]
 
 pub use self::{
     chromosome::*, crossover::*, individual::*, mutation::*, selection::*, statistics::*,
